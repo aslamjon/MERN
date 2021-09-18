642b8ce7b5bf207e4246ea93d3e3f639948292c5
aslamjon ibragimov <aslamjon.ibragimov@gmail.com>
2021-09-18 07:48:38 -0900

    complated landing page with responsive

---
+++ ReactJs/bit-bad/src/pages/LoginOrSignUpPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledLoginOrSignUp = styled.div`
+
+`;
+
+const LoginOrSignUpPage = () => {
+    return (
+        <StyledLoginOrSignUp>
+            LoginOrSignUpPage
+        </StyledLoginOrSignUp>
+    )
+}
+
+export default LoginOrSignUpPage
